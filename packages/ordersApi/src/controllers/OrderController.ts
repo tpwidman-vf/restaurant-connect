21f89e0cbf2413a9275360ffa273266cc1f3aff5
Taylor Widman <taylor.widman@voicefoundry.com>
2021-04-14 17:03:13 -0500

    Adds routes for getting a bunch of orders, and others by id

---
+++ packages/ordersApi/src/controllers/OrderController.ts
@@ -0,0 +1,139 @@
+import { injectable, inject } from "inversify"
+import { DataMapper } from '@aws/dynamodb-data-mapper';
+import { DocumentClient } from "aws-sdk/clients/dynamodb";
+
+import { 
+    apiController,
+    GET, 
+    POST,
+    PUT,
+    DELETE,
+    pathParam, 
+    queryParam, 
+    body
+ } from "ts-lambda-api";
+
+import { LoggerService } from "../utils/LoggerService";
+import { Order } from "../models/OrdersTable";
+import { RequestBody, QueryStringParams } from "../types/ApiTypes";
+import { isNil } from "lodash";
+
+@apiController("/orders")
+@injectable()
+export class OrderController {
+    /**
+     * 
+     * @param mapper 
+     * @param client 
+     * @param logger 
+     */
+    constructor(
+        @inject(DataMapper) private mapper: DataMapper,
+        @inject(DocumentClient) private client: DocumentClient,
+        @inject(LoggerService) private logger: LoggerService,
+    ) {}
+
+    /**
+     * 
+     * @param phoneNumber 
+     * @returns 
+     */
+    @GET("/")
+    public async findOrders(
+        @queryParam("phoneNumber") phoneNumber: string,
+        @queryParam("customer") customer: string,
+        @queryParam("orderStatus") orderStatus: string,
+    ){
+        const matchQueryParams = {
+            phoneNumber: (phoneNumber) ? `+${phoneNumber}` : undefined,
+            customer,
+            orderStatus
+        }
+        return await this.queryOrdersTable(matchQueryParams);
+    }
+    /**
+     * 
+     * @param matchQueryParams 
+     * @returns 
+     */
+    private async queryOrdersTable(matchQueryParams: QueryStringParams) {
+        console.log(matchQueryParams);
+        let baseQueryObject: DocumentClient.ScanInput = {
+            TableName: process.env.TABLE_NAME || "Orders", 
+            ExpressionAttributeNames: {},
+            ExpressionAttributeValues: {}, 
+            FilterExpression: ""
+        };
+
+        let query: any = Object.keys(matchQueryParams)
+            .filter((key) => !isNil(matchQueryParams[key as keyof QueryStringParams]))
+            .reduce((prev: any, current): any => {
+                const value = matchQueryParams[current as keyof QueryStringParams];
+                prev.ExpressionAttributeNames[`#${current}`] = current;
+                prev.ExpressionAttributeValues[`:${current}Value`] = value;
+                prev.FilterExpression += (prev.FilterExpression === "") ? `#${current} = :${current}Value` : ` AND #${current} = :${current}Value`
+                console.log(prev);
+                return prev;
+            }, baseQueryObject);
+        console.log(query);
+        const result = await this.client.scan(query).promise();
+        return result;
+    }
+
+    /**
+     * 
+     * @param requestBody 
+     * @returns 
+     */
+    @POST("/")
+    public async create(@body requestBody: RequestBody) {
+        if(!requestBody.createdAt){
+            requestBody.createdAt = new Date();
+        }
+        if(!requestBody.updatedAt){
+            requestBody.updatedAt = new Date();
+        }
+        return this.mapper.put(requestBody);
+    }
+
+    /**
+     * 
+     * @param id 
+     * @returns 
+     */
+    @GET("/:id")
+    public async getById(@pathParam("id") id: string) {
+        this.logger.info({ id }, "id");
+        return await this.mapper.get(Object.assign(new Order(), { orderId: id }));
+    }
+
+    /**
+     * 
+     * @param id 
+     * @param requestBody 
+     * @returns 
+     */
+    @PUT("/:id")
+    public async updateById(
+        @pathParam("id") id: string,
+        @body requestBody: RequestBody
+        ) {
+        if(!requestBody.updatedAt){
+            requestBody.updatedAt = new Date();
+        }
+        return this.mapper.put(Object.assign(new Order(), requestBody, { orderId: id }));
+    }
+
+    /**
+     * 
+     * @param id 
+     * @param requestBody 
+     * @returns 
+     */
+     @DELETE("/:id")
+     public async deleteById(
+         @pathParam("id") id: string,
+         ) {
+         return this.mapper.delete(Object.assign(new Order(), { orderId: id }));
+     }
+}

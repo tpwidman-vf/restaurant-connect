9d658b815d1c7d9753b9579ab1814d31cb3c7e31
Taylor Widman <taylor.widman@voicefoundry.com>
2021-04-15 16:20:29 -0500

    fixes issue with package json file

---
+++ packages/ordersApi/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  externals: {
+    'aws-sdk': 'aws-sdk'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.d\.ts$/,
+        loader: 'ignore-loader'
+      },
+      {
+        test: /\.js\.map$/,
+        loader: 'ignore-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'index.js',
+    chunkFilename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'commonjs2',
+    library: 'index',
+  },
+  target: 'node',
+};

60c7c5bcd458422b203768c2d5f6c1f9a80041ed
Taylor Widman <taylor.widman@voicefoundry.com>
2021-04-28 20:44:16 -0500

    Updates bash script for deleting localstack table

--- packages/ordersApi/test/createTables.sh
+++ packages/ordersApi/test/createTables.sh
@@ -3,3 +3,6 @@ aws --endpoint-url=http://localhost:4566 dynamodb create-table  \
 --attribute-definitions AttributeName=orderId,AttributeType=S \
 --key-schema AttributeName=orderId,KeyType=HASH \
 --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5
+
+aws --endpoint-url=http://localhost:4566 dynamodb delete-table  \
+--table-name Orders \

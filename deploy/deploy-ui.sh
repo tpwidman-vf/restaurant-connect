38f177398479276886ac1b07e764472fe08d3506
Alan Delimon <alan.delimon@voicefoundry.com>
2021-04-30 13:14:33 -0400

    Updated deploy-ui script to take commandline switches so --profile works for AWS

--- deploy/deploy-ui.sh
+++ deploy/deploy-ui.sh
@@ -2,4 +2,4 @@
 cd packages/restaurant-connect-ui
 export SKIP_PREFLIGHT_CHECK=true
 npm run build
-aws s3 cp ./build s3://orderstatus.vf-team8.com --recursive
+aws s3 cp ./build s3://orderstatus.vf-team8.com --recursive "$@"
